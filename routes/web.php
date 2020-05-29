4192807e25607ec7c675a4218cf6e6b483ecd0cc
mulananda <mulanandaa@gmail.com>
2020-05-30 00:40:14 +0700

    First Init

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+// $router->get('/', function () use ($router) {
+//     return $router->app->version();
+// });
+
+// /produk nama endpointnya yg akan digunakan (bebas nama endpointnya)
+$router->post('/produk', 'ProdukController@create');
+$router->get('/produk', 'ProdukController@index');
+$router->get('/produk/{id}', 'ProdukController@show');
+$router->put('/produk/{id}', 'ProdukController@update');
+$router->delete('/produk/{id}', 'ProdukController@destroy');
+
+$router->post('/register', 'UserController@register');
+$router->post('/login', 'UserController@login');
